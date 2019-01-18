8a99a5a18a93a5a89586a8b7f1715bc01890c5ea
Brainw8 <brainw8@gmail.com>
2019-01-18 11:13:12 +0300

    добавил JS код для модальных окон

--- source/js/main.js
+++ source/js/main.js
@@ -1,13 +1,63 @@
 "use strict"
 //modal window for services
+// var link = document.querySelector(".services a");
+// link.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   console.log("клик по сслке");
+//   popup.classList.add("login-popup-view");
+//   if (storage) {
+//     login.value = storage;
+//     password.focus();
+//   } else {
+//     login.focus();
+//   }
 
+// });
+// getAttribute("id")
 
+var nmodal = 0;
+var i;
+var link = document.querySelectorAll(".services__item a");
+var popup = document.querySelectorAll(".services__modal-window");
+var close = document.querySelectorAll(".services__modal-window-close");
+
+//var link = document.querySelector(".services a");
+link[0].addEventListener("click", function(event) {
+  event.preventDefault();
+  nmodal = 0;
+  //console.log("клик по ссылке");
+  popup[nmodal].classList.add("services__modal-window--show");
+  document.getElementById("overlay").style.display = "block";
+});
+link[1].addEventListener("click", function(event) {
+  event.preventDefault();
+  nmodal = 1;
+  //console.log("клик по ссылке");
+  popup[nmodal].classList.add("services__modal-window--show");
+  document.getElementById("overlay").style.display = "block";
+});
+link[2].addEventListener("click", function(event) {
+  event.preventDefault();
+  nmodal = 2;
+  //console.log("клик по ссылке");
+  popup[nmodal].classList.add("services__modal-window--show");
+  document.getElementById("overlay").style.display = "block";
+});
+
+for (i = 0; i < close.length; i++) {
+close[i].addEventListener("click", function(event) {
+  event.preventDefault();
+  document.getElementById("overlay").style.display = "none";
+  console.log("клик по сслке");
+  popup[nmodal].classList.remove("services__modal-window--show");
+});
+}
 
 
 //end
-// services gallery
-var largeImg = document.querySelector(".services__modal-main-photo");
-var thumbs = document.querySelector(".services__modal-items");
+// // services gallery
+var largeImg = document.querySelectorAll(".services__modal-main-photo");
+var thumbs = document.querySelectorAll(".services__modal-items");
 
 thumbs.onclick = function(e) {
   var target = e.target;
